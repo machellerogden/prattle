92d505131c19d324398836197038b22e401de5e6
Mac Heller-Ogden <mac@machellerogden.com>
2016-12-12 15:49:33 -0600

    adding test

---
+++ test-events/my-car-is-ford.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "session": {
+    "sessionId": "SessionId.8bf22b76-b6cc-4f18-922f-e48836f54dbf",
+    "application": {
+      "applicationId": "amzn1.ask.skill.b0598249-62b9-4853-ab9f-f822568647f8"
+    },
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account.AEG3W4YAT76QMRGZE35SYCATJXGGKYRBJYP64WIFSK7U6AQ54XLPQ4KSOIBUATSNHV7FUQLCDBP25KJ3RAXTR73OQSDVBMIFBOXZ7WXFPTX6APDMDUVKIHKFSHK6KVVU2ZNSJW3NZYBI6IY2INP35663TS55I47UGKMA2LUKR5KTQHC47WKD5YFCC7EU4UVXPG3KMTWBYSIO3AI"
+    },
+    "new": true
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId.16703cab-3099-43ba-9c47-05db17ccaeaa",
+    "locale": "en-US",
+    "timestamp": "2016-12-12T21:46:41Z",
+    "intent": {
+      "name": "MyCarIsIntent",
+      "slots": {
+        "Car": {
+          "name": "Car",
+          "value": "ford"
+        }
+      }
+    }
+  },
+  "version": "1.0"
+};
